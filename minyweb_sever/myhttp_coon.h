964c864768a4c2baebe4a7784ee7fb9d71dfe8b3
jialuhu <974520639@qq.com>
2018-09-13 16:10:23 +0800

    http请求解析的封装

--- minyweb_sever/myhttp_coon.h
+++ minyweb_sever/myhttp_coon.h
@@ -24,7 +24,7 @@ using namespace std;
 #define READ_BUF 2000
 class http_coon{
 public:
-    enum HTTP_CODE{NO_REQUESTION, GET_REQUESTION, BAD_REQUESTION, FORBIDDEN_REQUESTION,FILE_REQUESTION,INTERNAL_ERROR};
+    enum HTTP_CODE{NO_REQUESTION, GET_REQUESTION, BAD_REQUESTION, FORBIDDEN_REQUESTION,FILE_REQUESTION,INTERNAL_ERROR,NOT_FOUND};
     enum CHECK_STATUS{HEAD,REQUESTION};
 private:
     char requst_head_buf[1000];//响应头的填充
@@ -39,7 +39,12 @@ private:
     bool m_linger;//是否保持连接
     int m_http_count;//http长度
     char *m_host;
+    char path_400[17];
+    char path_403[23];
+    char path_404[40];
+    char message[1000];
     CHECK_STATUS status;
+    bool m_flag;
 public:
     int epfd;
     int client_fd;
@@ -62,6 +67,7 @@ private:
     bool bad_respond();
     bool forbiden_respond();
     bool succeessful_respond();
+    bool not_found_request();
    // void addfd(int e_fd, int client_fd, bool flag);
 };
 
@@ -133,15 +139,58 @@ int http_coon::myread()
 
 bool http_coon::succeessful_respond()
 {
-   // char *write_buf[2034];
-    sprintf(requst_head_buf,"HTTP/1.1 200 OK\r\nConnection: close\r\ncontent-length:%d\r\n\r\n",file_size);
+    m_flag = false;
+    bzero(requst_head_buf,sizeof(requst_head_buf));
+    sprintf(requst_head_buf,"HTTP/1.1 200 ok\r\nConnection: close\r\ncontent-length:%d\r\n\r\n",file_size);
 }
-bool http_coon::bad_respond()
+bool http_coon::bad_respond()//400
 {
-
+    bzero(url, strlen(url));
+    strcpy(path_400,"bad_respond.html");
+    url = path_400;
+    bzero(filename,sizeof(filename));
+    sprintf(filename,"/home/jialuhu/linux_net/web_sever/%s",url);
+    struct stat my_file;
+    if(stat(filename,&my_file)<0)
+    {
+        cout << "草拟\n";
+    }
+    file_size = my_file.st_size;
+    bzero(requst_head_buf,sizeof(requst_head_buf));
+    sprintf(requst_head_buf,"HTTP/1.1 400 BAD_REQUESTION\r\nConnection: close\r\ncontent-length:%d\r\n\r\n",file_size);
 }
-bool http_coon::forbiden_respond()
-{}
+bool http_coon::forbiden_respond()//403
+{
+    bzero(url, strlen(url));
+    strcpy(path_403,"forbidden_request.html");
+    url = path_403;
+    bzero(filename,sizeof(filename));
+    sprintf(filename,"/home/jialuhu/linux_net/web_sever/%s",url);
+    struct stat my_file;
+    if(stat(filename,&my_file)<0)
+    {
+        cout << "失败\n";
+    }
+    file_size = my_file.st_size;
+    bzero(requst_head_buf,sizeof(requst_head_buf));
+    sprintf(requst_head_buf,"HTTP/1.1 403 FORBIDDEN\r\nConnection: close\r\ncontent-length:%d\r\n\r\n",file_size);
+}
+bool http_coon::not_found_request()
+{
+    bzero(url, strlen(url));
+    strcpy(path_404,"not_found_request.html");
+    url = path_404;
+    bzero(filename,sizeof(filename));
+    sprintf(filename,"/home/jialuhu/linux_net/web_sever/%s",url);
+    struct stat my_file;
+    if(stat(filename,&my_file)<0)
+    {
+        cout << "草拟\n";
+    }
+    file_size = my_file.st_size;
+    bzero(requst_head_buf,sizeof(requst_head_buf));
+    sprintf(requst_head_buf,"HTTP/1.1 404 NOT_FOUND\r\nConnection: close\r\ncontent-length:%d\r\n\r\n",file_size);
+}
 void http_coon::doit()
 {
     cout << "doit\n";
@@ -151,22 +200,35 @@ void http_coon::doit()
     {
         case NO_REQUESTION:
         {
+            cout << "NO_REQUESTION\n";
             /*改变epoll的属性*/
             modfd(epfd, client_fd, EPOLLIN);
             return;
         }
-        case BAD_REQUESTION:
+        case BAD_REQUESTION: //400
         {
+            cout << "BAD_REQUESTION\n";
             bad_respond();
+            modfd(epfd, client_fd, EPOLLOUT);
             break;
         }
-        case FORBIDDEN_REQUESTION:
+        case FORBIDDEN_REQUESTION://403
         {
+            cout << "forbiden_respond\n";
             forbiden_respond();
+            modfd(epfd, client_fd, EPOLLOUT);
             break;
         }
-        case FILE_REQUESTION:
+        case NOT_FOUND://404
         {
+            cout<<"not_found_request"<< endl;
+            not_found_request();
+            modfd(epfd, client_fd, EPOLLOUT);
+            break;   
+        }
+        case FILE_REQUESTION://文件资源无问题
+        {
+            cout << "文件file request\n";
             succeessful_respond();
             modfd(epfd, client_fd, EPOLLOUT);
             break;
@@ -174,7 +236,7 @@ void http_coon::doit()
         default:
         {
             close_coon();
-        }
+    }
 
     }
 }
@@ -375,15 +437,16 @@ http_coon::HTTP_CODE http_coon::do_file()
     struct stat m_file_stat;
     if(stat(filename, &m_file_stat) < 0)
     {
-        return NO_REQUESTION;
+        cout << "打不开\n";
+        return NOT_FOUND;//NOT_FOUND 404
     }
-    if( !(m_file_stat.st_mode & S_IROTH))
+    if( !(m_file_stat.st_mode & S_IROTH))//FORBIDDEN_REQUESTION 403
     {
         return FORBIDDEN_REQUESTION;
     }
     if(S_ISDIR(m_file_stat.st_mode))
     {
-        return BAD_REQUESTION;
+        return BAD_REQUESTION;//BAD_REQUESTION 400
     }
     //int fd = open(filename, O_RDONLY);
     file_size = m_file_stat.st_size;
@@ -391,19 +454,53 @@ http_coon::HTTP_CODE http_coon::do_file()
 }
 bool http_coon::mywrite()
 {
+   /* if(m_flag)
+    {
+        int r;
+        int fd;
+        struct stat errno_file;
+        if(stat("not_found_request.html",&errno_file)<0)
+        {
+            return false;
+        }
+        file_size = errno_file.st_size;
+        fd = open("not_found_request.html",O_RDONLY);
+        assert(fd != -1);
+        r=send(client_fd,requst_head_buf,strlen(requst_head_buf),0);
+        cout << "^^^^r=" << r << endl;
+        if(r>0)
+        {
+            cout << "kjsljf\n";
+            r = sendfile(client_fd, fd, NULL, file_size);
+            cout << "sendfile-r:"<<r << endl;
+            if(r<=0)
+            {
+                cout << "失败\n";
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }*/
+    cout << "filename:"<<filename << endl;
     int fd = open(filename,O_RDONLY);
     assert(fd != -1);
     int ret;
     ret = write(client_fd,requst_head_buf,strlen(requst_head_buf));
+    cout << "client_fd:" << client_fd << " ret:" << ret << endl;
     if(ret < 0)
     {
+        close(fd);
         return false;
     }
     ret = sendfile(client_fd, fd, NULL, file_size);
+    cout << "ret:&&&" << ret << endl;
     if(ret < 0)
     {
+        close(fd);
         return false;
     }
+    close(fd);
     return true;
 }
 #endif
