457cdc1c36e08aa86c74a84d0a3fa6f41f8a0083
胡佳露 <974520639@qq.com>
2019-03-09 15:42:16 +0800

    download

--- minydownload/download.cpp
+++ minydownload/download.cpp
@@ -17,11 +17,12 @@
 #include<arpa/inet.h>
 #include<netdb.h>
 #include<assert.h>
+#include<stdlib.h>
 using namespace std;
 struct file_imformation{
     char *file_path;//文件的绝对路径
     char file_name[1000];//文件解析出来的名称
-    char *file_length;//文件的大小字节数目
+    long int file_length;//文件的大小字节数目
 };
 class Baseclient{
 private:
@@ -31,6 +32,7 @@ private:
     char *address;//下载地址参数
     char *fqdn;//FQDN解析
     char http_request[100];//http请求头填写
+    char http_respond[1000];//http响应头接收
     struct sockaddr_in server;//服务器套接字地址
     struct hostent *host;//通过解析下载地址，获取IP地址
     struct file_imformation myfile_information;//文件信息
@@ -46,7 +48,7 @@ public:
         bzero(&server,sizeof(server));
        bzero(&host,sizeof(host));
        bzero(&myfile_information,sizeof(myfile_information));
-        cout << address << "   " << thread_number << endl;
+ //       cout << address << "   " << thread_number << endl;
     };
     ~Baseclient();
     STATUS parse_address();//解析下载地址
@@ -57,18 +59,18 @@ public:
 
 Baseclient :: ~Baseclient()
 {
+    close(sockfd);
     delete [] myfile_information.file_path;
+
 
 }
 /*解析用户输入的下载地址*/
 Baseclient :: STATUS Baseclient :: parse_address()
 {
-    //int len = strlen(address);
     char *get;
     /*判断下载地址的状态*/
     if(strstr(address,"https") != NULL)
     {
-        //status = HTTPS;
         return HTTPS;
     }
     if(strstr(address, "http") != NULL)
@@ -81,15 +83,15 @@ Baseclient :: STATUS Baseclient :: parse_address()
     fqdn = get;//获取FQDN的起始位置
     get = strstr(get, "/");//解析出FQDN地址
     *get++ = '\0';
-    cout << "fqdn: " << fqdn << endl;
-    cout << "get: " << get << endl;
+    //cout << "fqdn: " << fqdn << endl;
+    host = gethostbyname(fqdn); //通过名字获取hostIP地址
     
     /*获取文件的绝对路径*/
     int len = strlen(get)+2;   
     myfile_information.file_path = new char(len);
     sprintf(myfile_information.file_path, "/%s",get);
     myfile_information.file_path[len-1] = '\0';
-    cout << myfile_information.file_path << endl;
+  //  cout << myfile_information.file_path << endl;
     len = strlen(myfile_information.file_path);
     
     /*获取文件原来的名称*/
@@ -107,9 +109,74 @@ Baseclient :: STATUS Baseclient :: parse_address()
     myfile_information.file_name[strlen(get)] = '\0';
     return HTTPS;
 }
+
+/*发送HTTP请求头，接受HTTP响应头，对头部内容进行解析*/
 void Baseclient :: parse_httphead()
 {
-
+    cout << "发送HTTP请求头：\n";
+    cout << http_request << endl;
+    cout << "接收HTTP响应头:\n";
+    int ret = write(sockfd, http_request, strlen(http_request));
+    if(ret <= 0)
+    {
+        cout << "wrong http_request\n";
+        exit(0);
+    }
+    int k = 0;
+    char ch[1];
+    /*解析出HTTP响应头部分*/
+    while(read(sockfd, ch, 1) != 0)
+    {
+        http_respond[k] = ch[0];
+        if(k>4 && http_respond[k]=='\n' && http_respond[k-1]=='\r' && http_respond[k-2]=='\n' && http_respond[k-3]=='\r')
+        {
+            break;
+        }
+        k++;
+    }
+    int len = strlen(http_respond);
+    http_respond[len] = '\0';
+    cout << http_respond<< endl;
+    
+    /*解析出content-length:字段*/
+    char *length;
+    length = strstr(http_respond,"Content-Length:");
+    if(length == NULL)
+    {
+        length = strstr(http_respond,"Content-length:");
+        if(length == NULL)
+        {
+            length = strstr(http_respond, "content-Length:");
+            if(length == NULL)
+            {
+                length = strstr(http_respond,"content-length:");
+                if(length == NULL)
+                {
+                    cout << "NO Content-Length\n";
+                    exit(0);
+                }
+            }
+        }
+    }
+    char *get = strstr(length,"\r");
+    *get = '\0';
+    length = length + 16;;
+    cout << length << endl;
+    long int t = atol(length);
+    myfile_information.file_length = t;
+    cout << t << endl;
+    int fd = open(myfile_information.file_name, O_CREAT | O_WRONLY, S_IRWXG | S_IRWXO | S_IRWXU);
+    assert(fd >= 0);
+    int index = 0;
+    int mycount = 0;
+    char buffer[1000];
+    while(mycount<myfile_information.file_length && (index = read(sockfd,buffer,1000))>0)
+    {
+        mycount = mycount + index; 
+        write(fd, buffer, index);
+    }
+    cout << "file_length::" << mycount<<endl;
+    close(fd);
 }
 void Baseclient :: thread_download()
 {
@@ -118,30 +185,28 @@ void Baseclient :: thread_download()
 void Baseclient :: mysocket()
 {
     parse_address();
-    cout << "8\n";
-    host = gethostbyname(fqdn);
-   /* if(host == NULL)
+    if(host == NULL)
     {
         status = HOST_WRONG;
-    }*/
+    }
     server.sin_family = AF_INET;
-   // server.sin_addr.s_addr = *(int *)host->h_addr_list[0]; 
-    //server.sin_port = htons(port);
+    server.sin_addr.s_addr = *(int *)host->h_addr_list[0]; 
+    server.sin_port = htons(port);
     /*创建套接字*/
-    //sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    //assert(sockfd>=0);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    assert(sockfd>=0);
     /*创建连接*/
-   // int ret = connect(sockfd, (struct sockaddr*)&server, sizeof(server));
-   // assert(ret != -1);
+   int ret = connect(sockfd, (struct sockaddr*)&server, sizeof(server));
+   assert(ret != -1);
 
     cout << "成功连接服务器!\n";
 
     /*填充HTTP请求头*/
-  /*  sprintf(http_request,"GET / %s HTTP/1.1\r\nHost: %s\r\nConnection: Close\r\n\r\n ",address, fqdn);
-   */
+    sprintf(http_request,"GET %s HTTP/1.1\r\nHost: %s\r\nConnection: Close\r\n\r\n ",myfile_information.file_path ,fqdn);
+    cout << "http_request:\n" << http_request << endl;
     /*分析收到的HTTP响应头*/
-    /*parse_httphead();
-*/
+    parse_httphead();
+
     /*根据线程数量进行下载文件*/
   //  thread_download();
     
@@ -150,7 +215,7 @@ void Baseclient :: mysocket()
 int main()
 {
     int thread_number = 8;
-    char address[100]="http//:st3.cdn.yestone.com/thumbs/2350753/image/15898/158987994/api_tumb_450.jpg";
+    char address[100]="http://pic1.sc.chinaz.com/files/pic/pic9/201903/zzpic16937.jpg";
     Baseclient myclient(thread_number, address);
    myclient.mysocket(); 
 }
