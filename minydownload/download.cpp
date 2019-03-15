8b1413317f925fdcb2f51dfbd0b72e256a8e4c78
胡佳露 <974520639@qq.com>
2019-03-15 17:50:41 +0800

    time

--- minydownload/download.cpp
+++ minydownload/download.cpp
@@ -31,6 +31,8 @@ struct thread_package{
     int start;//文件下载起始位置
     int end;//文件下载结束位置
     char file_name[1000];//文件名称
+    int read_ret;
+    int write_ret;
 };
 
 class Baseclient{
@@ -59,7 +61,7 @@ public:
         bzero(&Thread_package,sizeof(Thread_package));
        bzero(&host,sizeof(host));
        bzero(&myfile_information,sizeof(myfile_information));
-    };
+    }
     ~Baseclient();
     STATUS parse_address();//解析下载地址
     void parse_httphead();//解析HTTP响应头
@@ -67,6 +69,9 @@ public:
     void mysocket();
 private:
     static void *work(void *arg);
+public:
+    int byte_count;
+
 };
 
 Baseclient :: ~Baseclient()
@@ -204,12 +209,13 @@ void Baseclient :: parse_httphead()
 
 void* Baseclient :: work(void *arg)
 {
+    int s,e;
     char *buffer;
     struct thread_package *my = (struct thread_package *)arg;
    // cout << "myfile_information.file_name: " << my->file_name<<endl;
     int len = (my->end) - (my->start);
     buffer = new char[len];
-    cout << "len:" << len <<endl;
+    //cout << "len:" << len <<endl;
     int fd = open(my->file_name, O_CREAT | O_WRONLY, S_IRWXG | S_IRWXO | S_IRWXU);
    // cout << my->myfile_information.file_name_td << endl;
     assert(fd > 0);
@@ -219,23 +225,28 @@ void* Baseclient :: work(void *arg)
         cout << "lseek is wrong!\n";
     }
     int ave = len;
-    int read_ret=0;
-    int ret = 0;
-    int write_ret = 0;
-    while((ret = read(my->sockfd, buffer, len))>0 && read_ret!=ave)
+ //   int read_ret=0;
+    int r_ret = 0;
+    int w_ret = 0;
+    // int write_ret = 0;
+    s = clock();
+    while((r_ret = read(my->sockfd, buffer, len))>0 && my->read_ret!=ave)
     {
-        read_ret = read_ret + ret;
-        len = ave - read_ret;
-        write_ret = write(fd, buffer, ret);
-        cout << "write buf:" << write_ret << endl;
-    };
-    cout << "read_ret:" << read_ret << endl;
-    if(ret < 0)
+        my->read_ret = my->read_ret + r_ret;
+        len = ave - my->read_ret;
+        w_ret = write(fd, buffer, r_ret);
+        my->write_ret = my->write_ret + w_ret;
+       // cout << "write buf:" << write_ret << endl;
+    }
+   // cout << "read_ret:" << read_ret << endl;
+    if(r_ret < 0)
     {
         cout << "read is wrong!\n";
     }
     delete [] buffer;
     close(fd);
+    e = clock();
+    cout << "&&&&&&:" << e-s << endl;
     return 0;
 }
 void Baseclient :: thread_download()
@@ -256,18 +267,44 @@ void Baseclient :: thread_download()
         /*多线程下载*/
         for(i=0; i<thread_number; i++)
         {
+            Thread_package[i].read_ret = 0;
+            Thread_package[i].write_ret = 0;
             Thread_package[i].sockfd = sockfd;
             Thread_package[i].start = start;
             start += start + ave_bit;
             Thread_package[i].end = start;
-            strcpy(Thread_package[i].file_name, myfile_information.file_name);
+            //strcpy(Thread_package[i].file_name, myfile_information.file_name);
+            strcpy(Thread_package[i].file_name, myfile_information.file_name_td);
            // cout << "start:" << Thread_package[i].start << " " << "end: " << Thread_package[i].end << endl;
         }
+        //int start1;
+        //start1 = clock();
         for(i=0; i<thread_number; i++)
         {
             pthread_create(&pid, NULL, work, &Thread_package[i]);
             pthread_join(pid, &statu);    
         }
+            //pthread_join(pid, &statu);    
+
+        /*统计.*td文件中的字数是否等于总字符数量*/
+        int sum1 = 0;
+        int sum2 = 0;
+        for(auto i=0; i<thread_number; i++)
+        {
+            sum1 = sum1 + Thread_package[i].read_ret;
+            sum2 = sum2 + Thread_package[i].write_ret;
+        }
+       // int end;
+        if(sum1==sum2 && sum1==myfile_information.file_length)//标识着下载成功
+        {
+
+            rename(myfile_information.file_name_td, myfile_information.file_name);
+         //   end = clock();
+            cout << "下载完毕!\n";
+        }
+        //cout << "clock:" << end-start1 << endl;
+        //cout << "sum: " << sum1 << endl;
+        //cout << "sum: " << sum2 << endl;
         
     }
     
@@ -319,11 +356,21 @@ void Baseclient :: mysocket()
 
 int main(int argc, char const *argv[])
 {
-    int thread_number = 3;
+    int second1,second2;
+    //struct tm *p;
+    //time(&second);
+    //p = localtime(&second);
+    //second = mktime(p);
+   // second1 = clock();
+    //cout << "time:" << second1 << endl;
+    int thread_number = 1;
     char address[100]="http://pic1.sc.chinaz.com/files/pic/pic9/201903/zzpic16937.jpg";
     //char ad[100]="http://img.sccnn.com/bimg/341/11247.jpg";
     char ad[100]="http://jy.sccnn.com/zb_users/upload/2019/02/remoteimage2_20190215144726_32002.jpeg";
     //strcpy(add, argv[1]);
     Baseclient myclient(thread_number, ad);
    myclient.mysocket(); 
+    //second2 = clock();
+   // cout << "time:" << second2 << endl;
+    //cout << "result:" << second2-second1 << endl;
 }
