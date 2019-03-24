5151162556823f9e364455ede9a47dd9cddb850c
胡佳露 <974520639@qq.com>
2019-03-24 12:07:04 +0800

    main函数

--- minydownload/src/download_main.cpp
+++ minydownload/src/download_main.cpp
@@ -21,9 +21,9 @@ void menu()
     cout << "add:" << add << endl;
     cout << "请输入线程数量:\n";
     cin >> thread_number;
-    if(thread_number>20)
+    if(thread_number>20 || thread_number<=0)
     {
-        cout << "无法暴力下载,已使用默认线程数量下载\n";
+        cout << "该线程数量不符合范围,已使用默认线程数量下载\n";
         thread_number = 5;
     }
     Baseclient myclient(thread_number, add);
